5a044905d32629bf0be9a0784ec8088b7d3bc7a9
lvs007 <zy.liang.5@163.com>
2018-09-20 17:07:19 +0800

    add out message

--- check-machine-config.sh
+++ check-machine-config.sh
@@ -4,6 +4,12 @@ wget https://raw.githubusercontent.com/tronprotocol/TronDeployment/master/Benchm
 java -jar Benchmark.jar
 
 if [ $? == 1 ]; then
- echo "Start fail! Please upgrade server configuration." 
+ echo "-----------------------------------------------------"
+ echo "--------------------FAIL RESULT----------------------"
+ echo "Start fail! Please upgrade server configuration."
+ echo "CPU needs at least 16 cores"
+ echo "Memory needs at least 30GB"
+ echo "Java MUST be oracle jdk, and version >= 1.8"
+ echo "-----------------------------------------------------" 
  exit 1
 fi
